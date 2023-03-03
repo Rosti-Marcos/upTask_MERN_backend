3ff895dfb3d3a512cdbad41d9d7bc2dc5c9b36de
Marcos Rosti <rosti_marcos@hotmail.com>
2023-03-03 15:03:34 -0300

    cambio en variable de nuevo

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ dotenv.config();
 conectarDB();
 
 // Configurar CORS
-const whitelist = ["https://magnificent-platypus-42e1de.netlify.app/"];
+const whitelist = [process.env.FRONTEND_URL];
 
 const corsOptions = {
   origin: function (origin, callback) {
